92620cbb5e42bc664e17a3749316cfc314c5ea82
David Grant <davidgrant@gmail.com>
2015-11-01 20:59:40 -0800

    converted to python 3

--- euler01.py
+++ euler01.py
@@ -1,8 +1,10 @@
-def sumOfMultiples(max, multiple):
-    n = max/multiple
-    return multiple*n*(n+1)/2
+#!/usr/bin/env python3
 
-print sumOfMultiples(999,3) + sumOfMultiples(999,5) - sumOfMultiples(999,15)
+def sumOfMultiples(max, multiple):
+    n = max // multiple
+    return multiple*n*(n+1) // 2
+
+print(sumOfMultiples(999,3) + sumOfMultiples(999,5) - sumOfMultiples(999,15))
 
     
 

81d2002541e774b777f507e0da90c1309b59ab88
David Grant <davidgrant@gmail.com>
2018-05-06 23:52:43 -0700

    start of #24... not really much so far

---
+++ java/src/main/java/ca/davidgrant/euler24/Main.java
@@ -0,0 +1,47 @@
+package ca.davidgrant.euler24;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+public class Main {
+
+    // 0, 1
+
+    // 01
+    // 10
+
+    // 0, 1, 2
+
+    // 012
+    // 021
+    // 102
+    // 120
+    // 201
+    // 210
+
+    // 0, 1, 2, 3
+
+    // 0123
+    // 0132
+    // 0213
+    // 0231
+    // 0312
+    // 0321
+
+    // 1023
+    // 1032
+    // 1203
+    // 1231
+    // 1302
+    // 1320
+
+
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        
+    }
+}
